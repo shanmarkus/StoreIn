143dd1b109d2d190d2638ed17d4b03b6ce3d2f1b
shanmarkus <shanmarkus@gmail.com>
2014-03-27 20:43:08 +0700

    Adding Check In Fragment
    ++ fragment_check_in.xml

--- gen/com/example/storein/R.java
+++ gen/com/example/storein/R.java
@@ -1120,11 +1120,12 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int activity_sign_up=0x7f030019;
         public static final int fragment_browse=0x7f03001a;
-        public static final int fragment_home=0x7f03001b;
-        public static final int fragment_main=0x7f03001c;
-        public static final int fragment_navigation_drawer=0x7f03001d;
-        public static final int fragment_sign_up=0x7f03001e;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
+        public static final int fragment_check_in=0x7f03001b;
+        public static final int fragment_home=0x7f03001c;
+        public static final int fragment_main=0x7f03001d;
+        public static final int fragment_navigation_drawer=0x7f03001e;
+        public static final int fragment_sign_up=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int global=0x7f0c0000;
