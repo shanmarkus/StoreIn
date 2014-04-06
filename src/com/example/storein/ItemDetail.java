3af6ef9cbf98d6b90c1049a2381ec24ffab46092
shanmarkus <shanmarkus@gmail.com>
2014-04-06 20:16:23 +0700

    Adding View Page Adapter For Item Detail

--- src/com/example/storein/ItemDetail.java
+++ src/com/example/storein/ItemDetail.java
@@ -1,17 +1,27 @@
 package com.example.storein;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import com.example.storein.LocationCatalog.PlaceholderFragment.ImagePagerAdapter;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ItemDetail extends ActionBarActivity {
+
+	public static final String TAG = ItemDetail.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,13 @@ public class ItemDetail extends ActionBarActivity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		//UI Variable Declaration
+		ViewPager mViewPager;
+		Button mBtnLoveIt;
+		Button mBtnReviewIt;
+		Button mBtnCheckReview;
+		TextView mItemTitleLabel;
+		TextView mItemDescription;
 
 		public PlaceholderFragment() {
 		}
@@ -57,8 +74,60 @@ public class ItemDetail extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_item_detail,
 					container, false);
+			mViewPager = (ViewPager) rootView.findViewById(R.id.view_pager);
+			ImagePagerAdapter adapter = new ImagePagerAdapter();
+			mViewPager.setAdapter(adapter);
 			return rootView;
 		}
-	}
 
+		/*
+		 * Image Adapter Class
+		 */
+
+		public class ImagePagerAdapter extends PagerAdapter {
+			private int[] mImages = new int[] { R.drawable.chiang_mai,
+					R.drawable.himeji, R.drawable.petronas_twin_tower,
+					R.drawable.ulm };
+
+			@Override
+			public int getCount() {
+				return mImages.length;
+			}
+
+			@Override
+			public boolean isViewFromObject(View view, Object object) {
+				return view == ((ImageView) object);
+			}
+
+			@Override
+			public Object instantiateItem(ViewGroup container, int position) {
+				Context context = getActivity();
+				ImageView imageView = new ImageView(context);
+				int padding = context.getResources().getDimensionPixelSize(
+						R.dimen.padding_medium);
+				imageView.setPadding(padding, padding, padding, padding);
+				imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+				imageView.setImageResource(mImages[position]);
+
+				// Go to the gallery page of promotional
+				imageView.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						Log.d(TAG, "MAHO");
+
+					}
+				});
+				((ViewPager) container).addView(imageView, 0);
+				return imageView;
+			}
+
+			@Override
+			public void destroyItem(ViewGroup container, int position,
+					Object object) {
+				((ViewPager) container).removeView((ImageView) object);
+			}
+
+		}
+	}
 }
