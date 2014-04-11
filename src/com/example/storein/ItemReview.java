f6b82a7eaa4972265991d656e868db3ba98a413e
shanmarkus <shanmarkus@gmail.com>
2014-04-11 14:18:16 +0700

    Adding Method for getting item reviews

--- src/com/example/storein/ItemReview.java
+++ src/com/example/storein/ItemReview.java
@@ -1,22 +1,35 @@
 package com.example.storein;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.List;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class ItemReview extends ActionBarActivity {
+
+	// Variable
+	public static final String TAG = ItemReview.class.getSimpleName();
+	protected String itemId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_item_review);
+
+		itemId = getIntent().getExtras()
+				.getString(ParseConstants.KEY_OBJECT_ID);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -48,6 +61,8 @@ public class ItemReview extends ActionBarActivity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		// UI Declaration
+		ListView mListUsersReview;
 
 		public PlaceholderFragment() {
 		}
@@ -57,7 +72,28 @@ public class ItemReview extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_item_review,
 					container, false);
+			mListUsersReview = (ListView) rootView
+					.findViewById(R.id.listUsersReview);
 			return rootView;
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+		}
+
+		public void getUsersReview() {
+			ParseQuery<ParseObject> query = ParseQuery
+					.getQuery(ParseConstants.TABLE_ITEM_REVIEW);
+			query.whereEqualTo(ParseConstants.KEY_ITEM_ID, itemId);
+			query.findInBackground(new FindCallback<ParseObject>() {
+
+				@Override
+				public void done(List<ParseObject> review, ParseException e) {
+					// TODO Auto-generated method stub
+
+				}
+			});
 		}
 	}
 
