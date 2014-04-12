b0369395a5e47551f6983e7b6f8dfa226e346dbc
shanmarkus <shanmarkus@gmail.com>
2014-04-12 13:32:54 +0700

    Creating ListPromotion Class

--- gen/com/example/storein/R.java
+++ gen/com/example/storein/R.java
@@ -1210,20 +1210,20 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050026;
         public static final int action_bar_title=0x7f050025;
         public static final int action_context_bar=0x7f050020;
-        public static final int action_example=0x7f050062;
-        public static final int action_forgot_password=0x7f050063;
-        public static final int action_logout=0x7f050064;
+        public static final int action_example=0x7f050063;
+        public static final int action_forgot_password=0x7f050064;
+        public static final int action_logout=0x7f050065;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050027;
-        public static final int action_settings=0x7f050061;
+        public static final int action_settings=0x7f050062;
         public static final int activity_chooser_view_content=0x7f050028;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btnCheckReview=0x7f050052;
         public static final int btnLoveIt=0x7f05004e;
         public static final int btnReviewIt=0x7f050053;
-        public static final int btnSubmit=0x7f050060;
+        public static final int btnSubmit=0x7f050061;
         public static final int checkbox=0x7f050030;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003f;
@@ -1247,10 +1247,11 @@ containing a value of this type.
         public static final int image=0x7f05002a;
         public static final int itemDescriptionLabel=0x7f050050;
         public static final int itemTitleLabel=0x7f05004f;
-        public static final int lblReviewIt=0x7f05005e;
+        public static final int lblReviewIt=0x7f05005f;
         public static final int listItem=0x7f050055;
         public static final int listMode=0x7f050001;
         public static final int listPlace=0x7f05004a;
+        public static final int listPromotions=0x7f05005e;
         public static final int listUsersReview=0x7f050054;
         public static final int list_item=0x7f05002c;
         public static final int locationAddressLabel=0x7f050059;
@@ -1296,7 +1297,7 @@ containing a value of this type.
         public static final int terrain=0x7f05001b;
         public static final int title=0x7f05002e;
         public static final int top_action_bar=0x7f050023;
-        public static final int txtUserReview=0x7f05005f;
+        public static final int txtUserReview=0x7f050060;
         public static final int up=0x7f050024;
         public static final int useLogo=0x7f050003;
         public static final int usernameField=0x7f050040;
@@ -1359,19 +1360,21 @@ containing a value of this type.
         public static final int activity_location_detail=0x7f03001a;
         public static final int activity_login=0x7f03001b;
         public static final int activity_main=0x7f03001c;
-        public static final int activity_sign_up=0x7f03001d;
-        public static final int activity_write_review_item=0x7f03001e;
-        public static final int fragment_browse=0x7f03001f;
-        public static final int fragment_check_in=0x7f030020;
-        public static final int fragment_home=0x7f030021;
-        public static final int fragment_item_detail=0x7f030022;
-        public static final int fragment_item_review=0x7f030023;
-        public static final int fragment_location_catalog=0x7f030024;
-        public static final int fragment_location_detail=0x7f030025;
-        public static final int fragment_main=0x7f030026;
-        public static final int fragment_navigation_drawer=0x7f030027;
-        public static final int fragment_write_review_item=0x7f030028;
-        public static final int support_simple_spinner_dropdown_item=0x7f030029;
+        public static final int activity_promotion_list=0x7f03001d;
+        public static final int activity_sign_up=0x7f03001e;
+        public static final int activity_write_review_item=0x7f03001f;
+        public static final int fragment_browse=0x7f030020;
+        public static final int fragment_check_in=0x7f030021;
+        public static final int fragment_home=0x7f030022;
+        public static final int fragment_item_detail=0x7f030023;
+        public static final int fragment_item_review=0x7f030024;
+        public static final int fragment_location_catalog=0x7f030025;
+        public static final int fragment_location_detail=0x7f030026;
+        public static final int fragment_main=0x7f030027;
+        public static final int fragment_navigation_drawer=0x7f030028;
+        public static final int fragment_promotion_list=0x7f030029;
+        public static final int fragment_write_review_item=0x7f03002a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03002b;
     }
     public static final class menu {
         public static final int global=0x7f0d0000;
@@ -1382,8 +1385,9 @@ containing a value of this type.
         public static final int location_detail=0x7f0d0005;
         public static final int login=0x7f0d0006;
         public static final int main=0x7f0d0007;
-        public static final int sign_up=0x7f0d0008;
-        public static final int write_review_item=0x7f0d0009;
+        public static final int promotion_list=0x7f0d0008;
+        public static final int sign_up=0x7f0d0009;
+        public static final int write_review_item=0x7f0d000a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1429,8 +1433,8 @@ containing a value of this type.
         public static final int action_example=0x7f0a002d;
         public static final int action_forgot_password=0x7f0a003c;
         public static final int action_settings=0x7f0a002e;
-        public static final int action_sign_in_register=0x7f0a0055;
-        public static final int action_sign_in_short=0x7f0a0056;
+        public static final int action_sign_in_register=0x7f0a0056;
+        public static final int action_sign_in_short=0x7f0a0057;
         public static final int app_name=0x7f0a0027;
         /**   Title for notification shown when GooglePlayServices needs to be
         enabled for a application to work. [CHAR LIMIT=70] 
@@ -1529,10 +1533,10 @@ containing a value of this type.
          */
         public static final int common_signin_button_text_long=0x7f0a0020;
         public static final int email_hint=0x7f0a0036;
-        public static final int error_field_required=0x7f0a005b;
-        public static final int error_incorrect_password=0x7f0a005a;
-        public static final int error_invalid_email=0x7f0a0058;
-        public static final int error_invalid_password=0x7f0a0059;
+        public static final int error_field_required=0x7f0a005c;
+        public static final int error_incorrect_password=0x7f0a005b;
+        public static final int error_invalid_email=0x7f0a0059;
+        public static final int error_invalid_password=0x7f0a005a;
         public static final int error_title=0x7f0a0040;
         public static final int hello_blank_fragment=0x7f0a003e;
         public static final int hello_world=0x7f0a0031;
@@ -1546,7 +1550,7 @@ containing a value of this type.
         public static final int login_button_label=0x7f0a003a;
         public static final int login_error_message=0x7f0a0032;
         public static final int login_error_title=0x7f0a0033;
-        public static final int login_progress_signing_in=0x7f0a0057;
+        public static final int login_progress_signing_in=0x7f0a0058;
         public static final int love_it=0x7f0a004c;
         public static final int menu_logout_label=0x7f0a003b;
         public static final int navigation_drawer_close=0x7f0a002c;
@@ -1557,8 +1561,8 @@ containing a value of this type.
         public static final int post_out_range=0x7f0a003f;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f0a0053;
-        public static final int prompt_password=0x7f0a0054;
+        public static final int prompt_email=0x7f0a0054;
+        public static final int prompt_password=0x7f0a0055;
         public static final int sign_up_button_label=0x7f0a0037;
         public static final int sign_up_label=0x7f0a002f;
         public static final int signup_error_message=0x7f0a0038;
@@ -1568,7 +1572,8 @@ containing a value of this type.
         public static final int title_activity_item_review=0x7f0a0051;
         public static final int title_activity_location_catalog=0x7f0a0049;
         public static final int title_activity_location_detail=0x7f0a0044;
-        public static final int title_activity_login=0x7f0a0052;
+        public static final int title_activity_login=0x7f0a0053;
+        public static final int title_activity_promotion_list=0x7f0a0052;
         public static final int title_activity_sign_up=0x7f0a0030;
         public static final int title_activity_write_review_item=0x7f0a0050;
         public static final int title_section1=0x7f0a0028;
---
+++ src/com/example/storein/PromotionList.java
@@ -0,0 +1,82 @@
+package com.example.storein;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class PromotionList extends ActionBarActivity {
+	// Variables
+	public static final String TAG = PromotionList.class.getSimpleName();
+	protected String categoriesName;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_promotion_list);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.promotion_list, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+		// UI Variable
+		ListView mListPromotions;
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_promotion_list,
+					container, false);
+			return rootView;
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+		}
+
+		/*
+		 * Added Function
+		 */
+	}
+
+}
