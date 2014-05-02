f9d49bfeec30f43e10c7549be6015a011497a738
shanmarkus <shanmarkus@gmail.com>
2014-05-03 01:46:37 +0700

    adding CustomAdapter for Item

---
+++ src/com/example/storein/adapter/CustomArrayAdapterItem.java
@@ -0,0 +1,68 @@
+package com.example.storein.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.storein.R;
+import com.example.storein.model.Place;
+
+public class CustomArrayAdapterItem extends ArrayAdapter<Place> {
+
+	Context context;
+
+	public CustomArrayAdapterItem(Context context, int textViewResourceId,
+			List<Place> items) {
+		super(context, textViewResourceId, items);
+		this.context = context;
+	}
+
+	/* private view holder class */
+	private class ViewHolder {
+		TextView mTextPlaceName;
+		TextView mTextPlaceAddress;
+		ImageView mImagePromotionIcon;
+
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		Place record = (Place) getItem(position);
+
+		LayoutInflater mInflater = (LayoutInflater) context
+				.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.list_place_layout, null);
+			holder = new ViewHolder();
+			holder.mTextPlaceName = (TextView) convertView
+					.findViewById(R.id.placeName);
+			holder.mTextPlaceAddress = (TextView) convertView
+					.findViewById(R.id.placeAddress);
+			holder.mImagePromotionIcon = (ImageView) convertView
+					.findViewById(R.id.imagePromotionIcon);
+
+			convertView.setTag(holder);
+		} else
+			holder = (ViewHolder) convertView.getTag();
+
+		// show the data from database
+		holder.mTextPlaceName.setText(record.getName() + "");
+		holder.mTextPlaceAddress.setText(record.getAddress());
+		if (record.getIsPromotion() == true) {
+			holder.mImagePromotionIcon
+					.setImageResource(R.drawable.promotion_icon);
+		} else {
+			// do nothing
+		}
+
+		return convertView;
+	}
+}
