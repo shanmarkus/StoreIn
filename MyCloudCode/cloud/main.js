5e4b2080f06b723f19ddf794bb852a6b1ac2a573
shanmarkus <shanmarkus@gmail.com>
2014-05-08 20:13:03 +0700

    Adding Total Check In Place

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -5,7 +5,28 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+/* Start ------------------------------------------------ Calculation of Place Check In */
+Parse.Cloud.define("calculatePlaceCheckIn", function(request, response) {
+  var objectId = request.params.objectPlaceId;
+  var Place = Parse.Object.extend("Place");
+  var place = new Place();
+  place.id = objectId;
 
+  var query = new Parse.Query("Activity_User_Check_In_Place");
+  query.equalTo("placeId", place);
+  query.find({
+    success: function(results) {
+      response.success(results.length);
+    },
+    error: function() {
+      response.error("item lookup failed");
+    }
+  });
+});
+
+/* End ------------------------------------------------ Calculation of Place Check In */
+
+/* Start ------------------------------------------------ Calculation of User Check In */
 // Calculating Total Checkin 
 
 Parse.Cloud.define("calculateUserCheckIn", function(request, response) {
@@ -30,7 +51,9 @@ Parse.Cloud.define("calculateUserCheckIn", function(request, response) {
 Parse.Cloud.define("SaveTotalCheckIn",function(request, response) {
   // get Item Id
   var userId = request.params.objectId;
+  var placeId = request.params.objectPlaceId;
   var totalCheckIn;
+  var totalPlaceCheckIn;
 
   // get total Check in
   var CheckIn = Parse.Cloud.run('calculateUserCheckIn', {objectId:userId},{
@@ -63,13 +86,47 @@ Parse.Cloud.define("SaveTotalCheckIn",function(request, response) {
         console.error("Got an error " + error.code + " : " + error.message);
       }
     });
+
+  var PlaceCheckIn = Parse.Cloud.run('calculatePlaceCheckIn', {objectPlaceId:placeId},{
+    success: function(result) {
+    // getting the result
+    totalPlaceCheckIn = result;
+        // query
+        var Place = Parse.Object.extend("Place");
+        var query = new Parse.Query(Place);
+        query.equalTo("objectId", placeId);
+        query.find({
+          success: function(results) {
+            var obj = results[0];
+            obj.set("totalCheckIn", totalPlaceCheckIn);
+            obj.save(null,{
+              success: function (object) {
+                response.success(object);
+              },
+              error: function (object, error) {
+                response.error(error);
+              }
+            });
+          },
+          error: function(error) {
+            console.error("Got an error " + error.code + " : " + error.message);
+          }
+        });
+      },
+      error: function(error) {
+        console.error("Got an error " + error.code + " : " + error.message);
+      }
+    });
 });
 
-// Calculating after save item Review
+// Calculating after save User Check In to a place
 Parse.Cloud.afterSave("Activity_User_Check_In_Place", function(request, response) {
   var currentUser = Parse.User.current();
   var objId = currentUser.id;
-  Parse.Cloud.run("SaveTotalCheckIn",{ objectId: objId },{
+  var currentPlace = request.object.get("placeId");
+  var placeObjId = currentPlace.id;
+
+  Parse.Cloud.run("SaveTotalCheckIn",{ objectId: objId, objectPlaceId:placeObjId},{
     sucess: function(results) {
       response.success(results);
     },
@@ -79,6 +136,9 @@ Parse.Cloud.afterSave("Activity_User_Check_In_Place", function(request, response
   });
 });
 
+/* End ------------------------------------------------ Calculation of User Check In */
+
+/* Start ------------------------------------------------ Calculation of User Follower and Following  */
 
 // Finding user total follower of current User
 
@@ -185,6 +245,10 @@ Parse.Cloud.afterSave("Rel_User_User", function(request, response) {
   });
 });
 
+/* End ------------------------------------------------ Calculation of User Follower and Following  */
+
+/* Start ------------------------------------------------ Calculation of Average Rating Review Item  */
+
 // Finding Average Stars
 Parse.Cloud.define("averageRatings", function(request, response) {
   var query = new Parse.Query("ItemReview");
@@ -257,5 +321,6 @@ Parse.Cloud.afterSave("ItemReview", function(request, response) {
   });
 });
 
+/* End ------------------------------------------------ Calculation of Average Rating Review Item  */
 
 
